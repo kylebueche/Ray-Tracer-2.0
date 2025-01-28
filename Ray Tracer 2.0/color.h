e6198a8fcb4ab5088f94bfe8efa4f69a74e16446
Kyle Bueche <38002610+kylebueche@users.noreply.github.com>
2025-01-28 01:16:16 -1000

    Officially Tracing Rays Diffusely!

--- Ray Tracer 2.0/color.h
+++ Ray Tracer 2.0/color.h
@@ -1,6 +1,7 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "interval.h"
 #include "vec3.h"
 
 using color = vec3;
@@ -11,10 +12,11 @@ void write_color(std::ostream& out, const color& pixel_color)
 	auto g = pixel_color.y();
 	auto b = pixel_color.z();
 
-	// Turn [0f, 1f] into [0, 255]
-	int rbyte = int(255.99 * r);
-	int gbyte = int(255.99 * g);
-	int bbyte = int(255.99 * b);
+	// Translate the [0,1] component values into the byte range [0,255]
+	static const interval intensity(0.000, 0.999);
+	int rbyte = int(255.99 * intensity.clamp(r));
+	int gbyte = int(255.99 * intensity.clamp(g));
+	int bbyte = int(255.99 * intensity.clamp(b));
 
 	// Write out the pixel color components
 	out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
